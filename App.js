096d96b303a4e077428a794d49f15e2e35e1258a
LucassSantoss <lucascross.pereirasantos@gmail.com>
2026-03-03 20:06:12 -0300

    feat: implement database service  - create database  - create questions table  - insert questions  - get questions

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import {
+    createTableIfNotExists,
+    getQuestions,
+    insertQuestionsIfNotExists,
+} from "./src/db/sqliteDatabaseService";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [questions, setQuestions] = useState([]);
+
+    useEffect(() => {
+        const load = async () => {
+            await createTableIfNotExists();
+            await insertQuestionsIfNotExists();
+
+            const questions = await getQuestions();
+            setQuestions(questions);
+
+            console.log(questions);
+        };
+
+        load();
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
